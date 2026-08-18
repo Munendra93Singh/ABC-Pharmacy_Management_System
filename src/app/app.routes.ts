d3f07ff5fed5e9db59246c75eb4bbb3fa8a83821
Munendra Singh <munendrasingh93mahi@gmail.com>
2026-08-18 09:03:37 +0530

    Refactoring the code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,10 @@ import { Routes } from '@angular/router';
 import { AddMedicine } from './components/add-medicine/add-medicine';
 import { MedicineList } from './components/medicine-list/medicine-list';
 import { Home } from './components/home/home';
-import { Login } from './components/login/login';
-import { Registration } from './components/registration/registration';
-import { authGuard } from './Services/auth.guard';
+
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: Login },
-  { path: 'register', component: Registration },
-  { path: 'medicines', component: MedicineList, canActivate: [authGuard] },
-  { path: 'add-medicine', component: AddMedicine, canActivate: [authGuard] },
+  { path: '', redirectTo: 'medicines', pathMatch: 'full' },
+  { path: 'medicines', component: MedicineList},
+  { path: 'add-medicine', component: AddMedicine,},
 ];
